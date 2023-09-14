9db7fff6124b7faf7186015d27e13d19f73375bc
Dhruv Chandak <dhruvchandak5@gmail.com>
2023-09-14 22:40:43 +0530

    Added Offset

---
+++ frontend/src/components/Offset/Earth.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+import * as echarts from "echarts";
+import "echarts-gl";
+import earth from "./earth.jpg";
+
+const Earth = () => {
+  const chartRef = useRef([]);
+  useEffect(() => {
+    const chartDom = chartRef.current;
+    const myChart = echarts.init(chartDom);
+    const option = {
+      backgroundColor: "",
+      globe: {
+        baseTexture: earth,
+        shading: "lambert",
+        atmosphere: {
+          show: true,
+        },
+        light: {
+          ambient: {
+            intensity: 0.1,
+          },
+          displacmentQuality: "ultra",
+          main: {
+            intensity: 1,
+          },
+        },
+      },
+      series: [],
+    };
+    option && myChart.setOption(option);
+    return () => {
+      myChart.dispose();
+    };
+  }, []);
+  return <div ref={chartRef} style={{ width: "50vw", height: "80vh" }}></div>;
+};
+export default Earth;
