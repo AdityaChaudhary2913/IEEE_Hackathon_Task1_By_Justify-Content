2f5041e1553ca4fc563eaa3aba4c95f69619cd88
AdityaTheJaat <programmingID1306@gamil.com>
2023-09-16 22:53:32 +0530

    backend hosted

--- Backend/index.js
+++ Backend/index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/auth", userRoutes);
 
+app.get('/', (req, res) => {
+  res.send("IEEE Hackathon Backend Hosted Successfully!")
+})
+
 app.listen(8000, () => {
   console.log(`App is running at 8000`);
 });
